53b38d4b5c4ff6d8504b10bbdfacd4b297cee9d1
YifeiJing <jingyifei1998@qq.com>
2021-05-15 16:52:16 +0900

    edited myscript

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,1 @@
-curl --request GET "https://www.pref.fukuoka.lg.jp/contents/covid19-hassei.html" | grep '<strong>福岡地域' -A 14 | sed -e 's/<[a-z =:;"%0-9]*>//g' -e 's/<\/[a-z]*>//g' -e '/地域/d' -e '/県外/d' -e '/^$/d'
+curl --request GET "https://www.pref.fukuoka.lg.jp/contents/covid19-hassei.html" | grep '<strong>福岡地域' -A 99 | sed -e 's/<[a-z =:;"%0-9]*>//g' -e 's/<\/[a-z]*>//g' -e '/地域/d' -e '/県外/d' -e '/^$/d '
